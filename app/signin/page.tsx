f4cc6191f1f5df9411015cdd4e0ea2791a239977
Biplob Talukdar <biplob.codes@gmail.com>
2025-06-27 14:57:30 +0600

    Add heading to auth pages

---
+++ app/signin/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SigninPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-gray-600 font-semibold">
+        Welcome to the sign in Page
+      </h1>
+    </div>
+  );
+};
+
+export default SigninPage;
---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-gray-600 font-semibold">
+        Welcome to the sign up Page
+      </h1>
+    </div>
+  );
+};
+
+export default SignupPage;
