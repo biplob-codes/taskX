b52952f123b680200bb735040e35d7de5e763617
Biplob Talukdar <biplob.codes@gmail.com>
2025-06-27 15:14:33 +0600

    Add button to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,18 @@
-import Link from "next/link";
+import { Button } from "@radix-ui/themes";
+
 import React from "react";
 
 const NavBar = () => {
   return (
     <nav className="mb-5">
       <ul className="flex border-b border-gray-400 py-3 justify-between px-5">
-        <li className="text-gray-700">
-          <Link href="/">TaskX</Link>
+        <li className="text-gray-700">TaskX</li>
+        <li className="flex gap-3 items-center">
+          <Button>Signin</Button>
+          <Button color="pink" variant="soft">
+            Signup
+          </Button>
         </li>
-        <li>Profile</li>
       </ul>
     </nav>
   );
